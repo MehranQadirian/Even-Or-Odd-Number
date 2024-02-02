86edaa8ef20c4d092d0242ad12f95f31d5a36cb9
MehranQadirian <Mehranghadirian01@outlook.com>
2024-02-03 02:02:20 +0330

    Add Files

---
+++ CheckEvenOrOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Get number and check to this number is even or odd
+             */
+            Console.Write("Pls Enter Number: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number % 2 == 1)
+            {
+                Console.WriteLine("This Is Odd Number.");
+            }
+            else
+            {
+                Console.WriteLine("This Number is Even.");
+            }
+            Console.Write("Pls enter any key...");
+            Console.ReadKey();
+        }
+    }
+}
